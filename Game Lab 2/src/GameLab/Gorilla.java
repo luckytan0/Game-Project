c6448aea93c84697eb9865c658471fd359098abd
luckytan0 <157859645+luckytan0@users.noreply.github.com>
2024-11-25 19:22:49 -0500

    Add files via upload

---
+++ Game Lab 2/src/GameLab/Gorilla.java
@@ -0,0 +1,61 @@
+package Gaming;
+
+public class Gorilla extends NPC{
+	public Gorilla(String na, String de) {
+		super("Gorilla", "Donkey and Diddy Kong");
+	}
+	int count = 0;
+	@Override
+	public void talk() {
+		if(count==0) {
+			say("Hi, we're Donkey and Diddy Kong from Donkey Kong Country Returns");
+			String[] options = {
+					"Hey nice to meet you guys! I'm a huge fan of you guys video game",
+					"You guys are actual monkeys? No thank you"
+			};
+			getResponse(options);
+			count++;
+		}
+		else if(count==1) {
+			say("Hey would you guys tell us about your video game? Yes or No?");
+			String[] options = {
+					"Sure, but only if you give us our favorite fruit in bananas",
+					"No since you offended us we will not tell you about our video game."
+			};
+			getResponse(options);
+			count++;
+		}
+		else {
+			say("ooh ooh ah ah");
+			count++;
+		}
+	}
+	@Override
+	public void response(int option) {
+		if(count==0) {
+			switch(option) {
+			case 1:
+				say("It's nice meeting you too glad that you're our fan");
+				break;
+			case 2:
+				say("We are monkeys, got a problem with it?");
+				Game.print("The Kongs attacks you with their fist. You deserve it.");
+				break;
+			}
+		}
+		else if(count==1) {
+			switch(option) {
+			case 1:
+				say("Yes we will tell you about our video game.");
+				Game.print("All you have to do is collect as much bananas as you can in the game.");
+				Item banana = new Item("Banana", "A banana of currency");
+                Game.inventory.add(banana);
+				break;
+			case 2:
+				say("You're such a hater seriously");
+				Game.print("The Kongs stops talking.");
+				break;
+			}
+		}
+	}
+}
