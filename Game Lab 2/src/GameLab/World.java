101bdae6235e898e8d5f655c2de4270fb4de2899
luckytan0 <157859645+luckytan0@users.noreply.github.com>
2024-12-11 15:54:13 -0500

    Update World.java

--- Game Lab 2/src/GameLab/World.java
+++ Game Lab 2/src/GameLab/World.java
@@ -12,6 +12,8 @@ public class World {
 		Item book = new Item("ThomasCalculusBook", "Math Textbook");
 		Item marker = new Item("ExpoMarker", "Black Marker");
 		Item computer = new Item("Dell", "Desktop");
+		Item toy = new Item("toy", "puppy's favorite item");
+		Item currency = new Item("currency", "Donkey and Diddy Kong's banana grind");
 		Key key = new Key("SwordKey", "Sora's Key");
 		TeddyBear teddybear = new TeddyBear("TeddyBear", "Brown Teddy Bear");
 		TimeMachine timemachine = new TimeMachine("TimeMachine", "Reverse Time Machine");
@@ -30,23 +32,32 @@ public class World {
 		kapelski.addExit(kapelski3, 'n');
 		kapelski.addExit(kapelski4, 's');
 		kapelski.addExit(kapelski5, 'u');
-		kapelski.addExit(kapelski6, 'd');	
+		kapelski.addExit(kapelski6, 'd');
+		
 		kapelski1.addExit(kapelski, 'w');
-		kapelski2.addNPC("Puppy", puppy);
+		kapelski1.addNPC("Puppy", puppy);
+		kapelski1.addItem(toy.getName(), toy);
+		kapelski1.addNPC("Gorilla", gorilla);
+		kapelski1.addItem(currency.getName(), currency);
+		
 		kapelski2.addExit(kapelski, 'e');
 		kapelski2.addNPC("Gorilla", gorilla);
 		kapelski2.addItem(book.getName(), book);
 		kapelski2.addItem("SwordKey", key);
 		kapelski2.addExit(kapelski5, 'u');
+		
 		kapelski3.addExit(kapelski, 's');
 		kapelski3.addItem(marker.getName(), marker);
+		
 		kapelski4.addExit(kapelski, 'n');
 		kapelski4.addItem(computer.getName(), computer);
+		
 		kapelski5.addExit(kapelski, 'd');
 		kapelski5.addItem(combination.getName(),combination);
 		kapelski5.addItem("TeddyBear", teddybear);
 		kapelski5.addItem("TimeMachine", timemachine);
 		kapelski5.addExit(kapelski1, 'e');
+		
 		kapelski6.addExit(kapelski, 'u');
 		kapelski6.addItem(safe.getName(), safe);
 		return kapelski;
